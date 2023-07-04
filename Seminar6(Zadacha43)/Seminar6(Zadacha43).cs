bb31a5a8cf38363a2d562a36584fe339783d0533
unknown <valera.sant@mail.ru>
2023-07-04 13:10:29 +0300

    задача 43

---
+++ Seminar6(Zadacha43)/Seminar6(Zadacha43).cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите k1 и b1 для первого уравнения y=k1*x+b1, описывающего прямую на плоскости: ");
+double k1 = double.Parse(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2 и b2 для второго уравнения y=k2*x+b2, описывающего прямую на плоскости: ");
+double k2 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (k1 == k2) Console.WriteLine($"b1={b1}, k1={k1}; b2={b2}, k2={k2} --> Прямые не пересекаются");
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    Console.WriteLine($"x1 = x2 = {x}");
+    double y1 = k1 * x + b1;
+    Console.WriteLine($"y1 = {y1}");
+    double y2 = k2 * x + b2;
+    Console.WriteLine($"y2 = {y2}");
+    Console.WriteLine();
+    Console.WriteLine($"b1={b1}, k1={k1}; b2={b2}, k2={k2} --> ({x}; {k1 * x + b1})");
+}
